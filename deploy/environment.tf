b7d6cdcc65ab4f2a37a3f868bf709ba493dd32e1
Mihai Blebea <mihai@getchip.uk>
2020-11-07 18:39:23 +0000

    Adds terraform resources and files

---
+++ deploy/environment.tf
@@ -0,0 +1,19 @@
+variable "do_token" {
+    description = "Digital ocean auth token"
+}
+
+variable "cluster_name" {
+    description = "Name of the digital ocean kubernetes cluster"
+    default     = "mihaiblebea-platform-cluster"
+}
+
+variable "http_port" {
+    description = "Pod HTTP port"
+    default     = 8080
+}
+
+variable "node_port" {
+    default = 8080
+}
+
+variable "codebot_image" {}
---
+++ deploy/main.tf
@@ -0,0 +1,125 @@
+terraform {
+    required_version = "0.13.5"
+
+    required_providers {
+        digitalocean = {
+            source = "digitalocean/digitalocean"
+            version = "~> 1.22.0"
+        }
+    }
+    
+    backend "remote" {
+        organization = "PurpleTreeTech"
+
+        workspaces {
+            name = "codebot" // Add workspace to the terraform cloud
+        }
+    }
+}
+
+data "digitalocean_kubernetes_cluster" "platform_cluster" {
+    name = var.cluster_name
+}
+
+provider "kubernetes" {
+    load_config_file       = false
+    host                   = data.digitalocean_kubernetes_cluster.platform_cluster.endpoint
+    token                  = data.digitalocean_kubernetes_cluster.platform_cluster.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+        data.digitalocean_kubernetes_cluster.platform_cluster.kube_config[0].cluster_ca_certificate
+    )
+}
+
+
+resource "kubernetes_namespace" "codebot" {
+    metadata {
+        name = "codebot"
+    }
+}
+
+resource "kubernetes_deployment" "codebot_deployment" {
+    metadata {
+        name      = "codebot-deployment"
+        namespace = "codebot"
+        labels    = {
+            app  = "go-broadcast"
+            name = "codebot-deployment"
+        }
+    }
+
+    spec {
+        replicas = 1
+
+        selector {
+            match_labels = {
+                app  = "codebot"
+                name = "codebot-pod"
+            }
+        }
+
+        template {
+            metadata {
+                name   = "codebot-pod"
+                labels = {
+                    app  = "codebot"
+                    name = "codebot-pod"
+                }
+            }
+
+            spec {
+                container {
+                    image = var.codebot_image
+                    name  = "codebot-container"
+                    env {
+                        name  = "HTTP_PORT"
+                        value = var.http_port
+                    }
+                    port {
+                        container_port = var.http_port
+                    }
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service" "codebot_int" {
+    metadata {
+        name      = "codebot-service"
+        namespace = "codebot"
+    }
+
+    spec {
+        selector = {
+            name = "codebot-pod"
+        }
+
+        port {
+            port        = 80
+            target_port = var.http_port
+            node_port   = var.node_port
+        }
+
+        type = "NodePort"
+    }
+}
+
+resource "kubernetes_service" "codebot_ext" {
+    metadata {
+        name      = "codebot-service-ext"
+        namespace = "codebot"
+    }
+
+    spec {
+        selector = {
+            name = "codebot-pod"
+        }
+
+        port {
+            port        = 80
+            target_port = var.http_port
+        }
+
+        type = "ClusterIP"
+    }
+}
